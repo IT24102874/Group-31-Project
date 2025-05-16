c03ac05749c7577528f9d6500831fbd7701b6c20
dilu <it24102876@my.sliit.lk>
2025-05-16 14:40:39 +0530

    Doctor.jsp

--- src/main/java/Doctor.java
+++ src/main/java/Doctor.java
@@ -1,2 +1,44 @@
 public class Doctor {
+
+
+    private String username;
+    private String name;
+    private String email;
+    private String password;
+    private String location;
+
+    public Doctor(String username, String name, String email, String password, String location) {
+        this.username = username;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.location = location;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+
+
+
+
+
+
 }
