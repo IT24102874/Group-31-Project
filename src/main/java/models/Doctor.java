74fb9c395ea57e12ace1738f044c7fbe4320ba82
dilu <it24102876@my.sliit.lk>
2025-05-21 11:40:11 +0530

    Doctor

--- src/main/java/models/Doctor.java
+++ src/main/java/models/Doctor.java
@@ -1,4 +1,43 @@
 package models;
 
-public class Doctor {
+import utils.AppointmentFileHandler;
+import models.Appointment;
+import java.io.IOException;
+import java.util.List;
+
+public class Doctor extends User {
+    private String location;
+    private String specialization;
+    private String contact;
+
+    public Doctor(String name, String phoneNumber, String email, String userName, String password, String location) {
+        super(name, phoneNumber, email, "Doctor", userName, password);
+        this.location = location;
+    }
+
+    public Doctor(String username, String password, String name, String email) {
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.specialization = "";
+        this.contact = "";
+    }
+
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Appointment> getMyAppointments() {
+        try {
+            return AppointmentFileHandler.getAppointmentsByDoctorName(this.getName());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
