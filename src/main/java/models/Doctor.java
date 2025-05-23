f1015367bff8f2b7bd4588027fa4843039c943a9
dilu <it24102876@my.sliit.lk>
2025-05-23 07:22:34 +0530

    Doctor

--- src/main/java/models/Doctor.java
+++ src/main/java/models/Doctor.java
@@ -25,10 +25,12 @@ public class Doctor extends User {
 
 
     public String getLocation() {
+
         return location;
     }
 
     public void setLocation(String location) {
+
         this.location = location;
     }
 
